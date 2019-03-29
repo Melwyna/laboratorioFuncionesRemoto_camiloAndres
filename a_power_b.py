816afafb2d5132edadad5925ef66fac82df1e0e6
Melwyna <47457927+Melwyna@users.noreply.github.com>
2019-03-29 14:12:32 -0500

    Add files via upload

--- a_power_b.py
+++ a_power_b.py
@@ -1,11 +1,20 @@
 def a_power_b(a,b):
+
     cont=1
 
     for i in range(0,b):
         cont=(cont*a)
+
+    return cont
   
 
-a=int(input("dijite el numero que quieres elevar"))
-b=int(input("dijite el numero elevado"))
-res=a_power_b(a,b)
-print(res)
+while True:
+
+    a=int(input("dijite el numero que quieres elevar"))
+
+    if a==0:
+        break
+
+    b=int(input("dijite el numero elevado"))
+    res=a_power_b(a,b)
+    print(res)
