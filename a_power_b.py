fccab0ee44da3bd081cef04b215c986fc71671cd
Melwyna <47457927+Melwyna@users.noreply.github.com>
2019-03-29 13:29:55 -0500

    Update a_power_b.py

--- a_power_b.py
+++ a_power_b.py
@@ -1,9 +1,11 @@
-def elevado():
+def a_power_b(a,b):
     cont=1
-    a=int(input("dijite el numero que quieres elevar"))
-    b=int(input("dijite el numero elevado"))
+
     for i in range(0,b):
         cont=(cont*a)
-    print(cont)
+  
 
-elevado()
+a=int(input("dijite el numero que quieres elevar"))
+b=int(input("dijite el numero elevado"))
+res=a_power_b(a,b)
+print(res)
